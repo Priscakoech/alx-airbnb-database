50fb3c3aa28446111f15ea81b5fe69ae284e0c91
Priscakoech <priscakoech448@gmail.com>
2025-07-05 18:33:59 +0300

    added the sub queries

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -1,2 +1,6 @@
 ## Advanced SQL queries 
 this folder contains advanced sql queries using INNER JOIN, LEFT JOIN and FULL OUTER JOIN 
+
+## Subqueries
+they are both correlated and non-correlated subqueries
+they find all the properties where the average rating is greater than 4.0 and the other to find users who made more than 3 bookings.
---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,35 @@
+SELECT
+property_id
+name,
+location,
+
+FROM
+Property
+
+WHERE
+property_id IN (
+    SELECT
+        property_id
+    FROM
+        Review
+    HAVING 
+        AVG(rating) > 4.0
+);
+
+SELECT
+user.user_id,
+user.first_name,
+user.last_name,
+user.email,
+
+FROM
+User
+WHERE
+(
+    SELECT
+        COUNT(*)
+    FROM
+        Booking
+    WHERE
+        Booking.user_id = User.user_id  
+) > 5
